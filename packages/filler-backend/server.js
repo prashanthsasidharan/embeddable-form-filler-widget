f57a3f1c884ea9924b0f662b10d367ab07dd0793
prashanth <prashanth.ss@zohocorp.com>
2023-01-10 12:46:54 +0530

    added cors for apis

--- packages/filler-backend/server.js
+++ packages/filler-backend/server.js
@@ -2,10 +2,12 @@ require('dotenv').config()
 let express = require('express');
 let mongoose = require('mongoose');
 let fillerRouter = require('./routers/filler');
+let cors = require('cors');
 
 let app = express();
 mongoose.connect(process.env.DATABASE_URL);
 
+app.use(cors());
 let db = mongoose.connection;
 db.on('error', (err) => console.log(err));
 db.once('open', () => console.log('db started'))
